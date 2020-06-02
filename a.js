7d3a52457403da6415fcdc9d32ab08e22b875454
Magnús Ólafsson <magnusol93@gmail.com>
2020-06-02 20:04:54 +0200

    crap commit

--- a.js
+++ a.js
@@ -1,3 +1,63 @@
 console.log("This is the initial commit");
 
 console.log("commiting on master");
+
+console.log("Different comments");
+console.log("Different comments");
+console.log("Different comments");
+console.log("Different comments");
+
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+console.log("from master");
+
+
+
+
+
+console.log('SHOULD BE DELETED')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+----------------------
+
+
+
+
+
+
+
+
