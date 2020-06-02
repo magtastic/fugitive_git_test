6ecb3caa41d83ae882cca49612dc76f9ce18511e
Magnús Ólafsson <magnusol93@gmail.com>
2020-06-02 19:48:43 +0200

    crap commit from b

--- a.js
+++ a.js
@@ -1,3 +1,34 @@
-console.log("This is the initial commit");
+console.log("This is the initial commit from branch b");
 
 console.log("commiting on master");
+
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+
+o;
+
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
+console.log("from branch b");
